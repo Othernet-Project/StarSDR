52d49c8e61c655e07ce6761314e6e8fc4cf5afb0
abhishek <abhishek@outernet.is>
2016-07-27 21:33:33 +0530

    fix librtlsdr - missing symbols

--- build.sh
+++ build.sh
@@ -3,14 +3,19 @@ export CROSS_COMPILE="${PREFIX}/arm-buildroot-linux-gnueabihf-"
 export OUTDIR="$PWD/out/"
 [ -d "$OUTDIR" ] || mkdir "$OUTDIR"
 
+LIBUSB_CFLAGS=$(${PREFIX}pkg-config libusb-1.0 --cflags)
+LIBUSB_LFLAGS=$(${PREFIX}pkg-config libusb-1.0 --libs)
+
 # build libmirisdr
 pushd libmirisdr-2/src
-${CROSS_COMPILE}gcc -Wall -O3 -fPIC -shared -o ${OUTDIR}libmirisdr.so -I../include `${PREFIX}pkg-config libusb-1.0 --cflags` libmirisdr.c `${PREFIX}pkg-config libusb-1.0 --libs`
+FILES="libmirisdr.c"
+${CROSS_COMPILE}gcc -Wall -O3 -fPIC -shared -o ${OUTDIR}libmirisdr.so -I../include ${LIBUSB_CFLAGS} ${FILES} ${LIBUSB_LFLAGS}
 popd
 
 # build librtlsdr
 pushd librtlsdr/src
-${CROSS_COMPILE}gcc -Wall -O3 -fPIC -shared -o ${OUTDIR}librtlsdr.so -I../include `${PREFIX}pkg-config libusb-1.0 --cflags` librtlsdr.c `${PREFIX}pkg-config libusb-1.0 --libs`
+FILES="librtlsdr.c tuner_e4k.c tuner_fc0012.c tuner_fc0013.c tuner_fc2580.c tuner_r82xx.c"
+${CROSS_COMPILE}gcc -Wall -Wno-unused-function -Wno-unused-variable -O3 -fPIC -shared -o ${OUTDIR}librtlsdr.so -I../include ${LIBUSB_CFLAGS} ${FILES} ${LIBUSB_LFLAGS}
 popd
 
 # build starsdr
